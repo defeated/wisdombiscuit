a2f1a95e06786392de9618dc22b23a5265b4e64f
Eddie Cianci <defeated2k4@gmail.com>
2010-08-07 00:39:10 -0400

    new rails 3 app using RSpec and Cucumber

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.0.rc'
+
+gem 'sqlite3-ruby', :require => 'sqlite3'
+
+group :test do
+  gem 'rspec',          '>= 2.0.0.beta.19'
+  gem 'rspec-rails',    '>= 2.0.0.beta.19'
+  gem 'cucumber',       '>= 0.8.5'
+  gem 'cucumber-rails', '>= 0.3.2'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wisdombiscuit::Application.config.secret_token = 'a6031ff07a34770bba85daf565e030a6b90afa7970c998ec01d3b89bb816943ca1fb5700146e479ba5338018d8142e953bb137543fbe9ae5dd5fcaec3b78b839'
