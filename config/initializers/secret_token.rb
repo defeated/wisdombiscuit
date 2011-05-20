1961e9fc810f72e811eead780f7e90f1ba8856af
eddie cianci <defeated2k4@gmail.com>
2011-05-19 23:14:10 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wisdombiscuit::Application.config.secret_token = '3fc48a42ede90e92f60e1cf1f9309605abf140943f3f6b50bd066acdc50aa46ed661b34bd64b73bb76fd257d06367e9b8ee4b89fafa73cd0b69819543fb30fd3'
