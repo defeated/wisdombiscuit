882b86db17775060345d601ab9ab1366f80ae4f3
eddie cianci <defeated2k4@gmail.com>
2011-05-22 02:34:28 -0400

    get new random fortune via ajax

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,13 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(function(){
+
+	$('#again').bind('ajax:success', function(evt, data){
+		var fortune = $("#fortune");
+		fortune.fadeOut('slow', function(){
+			fortune.html(data.fortune.text).fadeIn('slow');
+		})
+	});
+
+});
