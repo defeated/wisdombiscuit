fa840c923b3cb74f30f0dbf73f242cae84430220
eddie cianci <defeated2k4@gmail.com>
2011-06-01 21:36:15 -0400

    changed scaffolded controller to new respond_with() syntax

--- app/controllers/fortunes_controller.rb
+++ app/controllers/fortunes_controller.rb
@@ -1,58 +1,45 @@
 class FortunesController < ApplicationController
   before_filter :authenticate_user!
 
+  respond_to :html, :xml, :json
+
   # GET /fortunes
   # GET /fortunes.xml
   def index
     @fortunes = Fortune.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @fortunes }
-    end
+    respond_with @fortunes
   end
 
   # GET /fortunes/1
   # GET /fortunes/1.xml
   def show
     @fortune = Fortune.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @fortune }
-    end
+    respond_with @fortune
   end
 
   # GET /fortunes/new
   # GET /fortunes/new.xml
   def new
     @fortune = Fortune.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @fortune }
-    end
+    respond_with @fortune
   end
 
   # GET /fortunes/1/edit
   def edit
     @fortune = Fortune.find(params[:id])
+    respond_with @fortune
   end
 
   # POST /fortunes
   # POST /fortunes.xml
   def create
     @fortune = Fortune.new(params[:fortune])
-
-    respond_to do |format|
-      if @fortune.save
-        format.html { redirect_to(@fortune, :notice => 'Fortune was successfully created.') }
-        format.xml  { render :xml => @fortune, :status => :created, :location => @fortune }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @fortune.errors, :status => :unprocessable_entity }
-      end
+    
+    if @fortune.save
+      flash[:notice] = 'Fortune was successfully created.'
     end
+  
+    respond_with @fortune
   end
 
   # PUT /fortunes/1
@@ -60,15 +47,11 @@ class FortunesController < ApplicationController
   def update
     @fortune = Fortune.find(params[:id])
 
-    respond_to do |format|
-      if @fortune.update_attributes(params[:fortune])
-        format.html { redirect_to(@fortune, :notice => 'Fortune was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @fortune.errors, :status => :unprocessable_entity }
-      end
+    if @fortune.update_attributes(params[:fortune])
+      flash[:notice] = 'Fortune was successfully updated.'
     end
+
+    respond_with @fortune
   end
 
   # DELETE /fortunes/1
@@ -76,10 +59,7 @@ class FortunesController < ApplicationController
   def destroy
     @fortune = Fortune.find(params[:id])
     @fortune.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(fortunes_url) }
-      format.xml  { head :ok }
-    end
+    flash[:notice] = 'Fortune was successfully destroyed.'
+    respond_with @fortune
   end
 end
